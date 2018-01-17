4f36eb863c5cd3567b012ee43035f390f8d9f7f4
darthjee <darthjee@gmail.com>
2018-01-17 23:10:46 +0100

    Cashe view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,17 @@ class ApplicationController < ActionController::Base
     action = params[:action]
     respond_to do |format|
       format.json { render json: send("#{action}_json") }
-      format.html { render action }
+      format.html { cached_render action }
     end
   end
 
   def not_found
     head :not_found
+  end
+
+  def cached_render(view)
+    Rails.cache.fetch "render:#{params[:controller]}:#{view}" do
+      render view
+    end
   end
 end
